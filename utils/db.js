874440e577352c1597c200fbb059b0fbd1633d01
Rahima <rahimasarwar29@gmail.com>
2025-08-26 11:55:52 +0500

    db.js file update for vercel

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://rahimasarwar29:riuPrs5w1J6pkMfu@cluster0.cuye5nt.mongodb.net/smartstock?retryWrites=true&w=majority"
+const MONGODB_URI = process.env.MONGODB_URI; // .env se lo
 
 if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in .env.local");
+  throw new Error("Please define MONGODB_URI in environment variables");
 }
 
 let cached = global.mongoose;
@@ -16,11 +16,11 @@ export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false, // Important for Vercel
+    }).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
-
